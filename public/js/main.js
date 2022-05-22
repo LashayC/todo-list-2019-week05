a1a7f52588a557339a4ebddb185d18a6149890cd
LashayC <cokleylashay@gmail.com>
2022-05-22 17:24:30 -0400

    node updates

--- public/js/main.js
+++ public/js/main.js
@@ -1,121 +1,104 @@
-// Assign variables for the html elements so that there's less redundant typing in the function.
-//create a function
-//add event object so it doesn't refresh
-//stop function from running with input is empty
-//get the input
-//create the li
-//put input in the li
-//append li to the ul
-//reset input field so it's empty
+//const bodyParser = require("body-parser")
+const li = document.querySelectorAll("li");
+const listItems = document.querySelectorAll(".listItems");
+const checkList = document.querySelectorAll(".checkList");
+// console.log(`these are the spans ${listItems[2].innerText} `)
+// console.log(`these are the check spans ${checkList.innerText} `)
 
-const bodyParser = require("body-parser")
+const clearAllBtn = document.getElementById("clearBtn");
+const clearCrossedBtn = document.getElementById("clearCrossed");
+// const listCount = document.getElementById('listCount')
 
-// Assign variables for the html elements so that there's less redundant typing in the function.
-const input = document.getElementById('todoInput')
-const button = document.getElementById('submitBtn')
-const ul = document.getElementById('todoList')
-const clearAllBtn = document.getElementById('clearBtn')
-const clearCrossedBtn = document.getElementById('clearCrossed')
-const listCount = document.getElementById('listCount')
-
-//create a function
-// function toDoList(e){
-//     // add event object so it doesn't refresh
-//     e.preventDefault()
-    
-//     // stop function from running with input is empty
-//     if(input.value === ""){
-//         return
-//     }
-    
-//     //get the input
-//     let text = input.value
-    
-//     //create the li
-//     const li = document.createElement('li')
-  
-//     li.className = 'notcrossed'
-
-//     //put input in the li
-//     li.innerText = text
-    
-//     //append li to the ul
-//     ul.appendChild(li)
-    
-//     //reset input field so it's empty
-//     input.value = ""
-    
 //     let count = document.querySelectorAll('.notcrossed').length
 //     listCount.innerText = count
 // }
+//     let nc = document.querySelectorAll('.notcrossed').length
 
-document.querySelector('h1').innerText = 'caydaddy'
-function makeCrossedList(e){
+//     listCount.innerText = nc
+// }
 
-    if(e.target.classList.contains('notcrossed')){
-        e.target.className = 'crossed'
-        e.target.style.textDecoration = 'line-through'
-        e.target.style.color = 'gray'
-
-    }else if(e.target.classList.contains('crossed')){
-        e.target.className = 'notcrossed'
-        e.target.style.textDecoration = 'none'
-        e.target.style.color = 'black'
-
+function addAndRemoveStrikethrough() {
+  li.forEach((element) => {
+    let items = element.childNodes[1]; //all of the spans
+    let checkedOff = element.childNodes[3].innerText; //all of the spans with checked
+    //console.log(checkedOff == 'no')
+    //console.log(element.childNodes[1].style.color = 'red')
+    //checkedOff.style.color = 'red'
+    if (checkedOff == "yes") {
+      items.style.textDecoration = "line-through";
+      items.style.color = "gray";
+    } else if (checkedOff == "no") {
+      items.style.textDecoration = "none";
+      items.style.color = "black";
     }
-  
-    let nc = document.querySelectorAll('.notcrossed').length
-
-    listCount.innerText = nc
+  });
 }
 
-// function clearAllList(){
-//     while(ul.firstChild){
-//         ul.removeChild(ul.firstChild)
-//         listCount.innerText  = "0"
-//     }
-// }
+addAndRemoveStrikethrough();
 
-// function clearCrossedList(){
-//     Array.from(ul.childNodes).forEach( el => {
-//         console.log(el)
-//         if(el.classList.contains('crossed')){
-//             el.remove()
-//         }
-//     })
-// }
+Array.from(listItems).forEach(function (element) {
+  element.addEventListener("click", function () {
+    const item = this.parentNode.childNodes[1].innerText; //todo items
+    const checkedOff = this.parentNode.childNodes[3].innerText; //checkedOff yes or no's
+    console.log(`here ${checkedOff}`);
+    console.log(this);
 
+    //conditional for adding yes or no to checkedoff property. Goes with 2 dif PUTS. **Check if thats bad code with EIR
+    if (checkedOff == "no") {
+      fetch("crossedOut", {
+        method: "put",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          item: item,
+          checkedOff: checkedOff,
+        }),
+      })
+        .then((response) => {
+          if (response.ok) return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          window.location.reload(true);
+        });
+    } else if (checkedOff == "yes") {
+      fetch("notCrossedOut", {
+        method: "put",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          item: item,
+          checkedOff: checkedOff,
+        }),
+      })
+        .then((response) => {
+          if (response.ok) return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          window.location.reload(true);
+        });
+    } //else statement
+  }); //event listener
+}); //forEach function
 
+//clear checkedOff:yes list items
+clearCrossedBtn.addEventListener("click", (_) => {
+  fetch("deleteCrossedOut", {
+    method: "delete",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      checkedOff: "yes",
+    }),
+  }).then(function (response) {
+    window.location.reload();
+  });
+});
 
-
-//add event listener
-button.addEventListener('click', toDoList)
-
-ul.addEventListener('click', makeCrossedList)
-
-clearAllBtn.addEventListener('click', clearAllList)
-
-clearCrossedBtn.addEventListener('click',clearCrossedList )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+clearAllBtn.addEventListener("click", (_) => {
+  fetch("deleteAll", {
+    method: "delete",
+  }).then(function (response) {
+    window.location.reload();
+  });
+});
